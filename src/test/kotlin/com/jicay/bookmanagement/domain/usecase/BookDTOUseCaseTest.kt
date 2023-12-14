8c15e5f005ad8831ac00db379aa605202b101fb9
Terencyl <lucas40110@outlook.fr>
2023-12-14 12:16:07 +0100

    Ajout T.U. pour BookDTOUseCase et ajout T.I.

--- src/test/kotlin/com/jicay/bookmanagement/domain/usecase/BookDTOUseCaseTest.kt
+++ src/test/kotlin/com/jicay/bookmanagement/domain/usecase/BookDTOUseCaseTest.kt
@@ -1,7 +1,12 @@
 package com.jicay.bookmanagement.domain.usecase
 
+import assertk.assertFailure
 import assertk.assertThat
 import assertk.assertions.containsExactly
+import assertk.assertions.isFalse
+import assertk.assertions.isInstanceOf
+import assertk.assertions.isTrue
+import com.jicay.bookmanagement.domain.exceptions.BookReservedException
 import com.jicay.bookmanagement.domain.model.Book
 import com.jicay.bookmanagement.domain.port.BookPort
 import io.mockk.every
@@ -46,5 +51,63 @@ class BookDTOUseCaseTest {
         bookUseCase.addBook(book)
 
         verify(exactly = 1) { bookPort.createBook(book) }
+    }
+
+    @Test
+    fun `reserve book`() {
+        var existingBook = Book("test", "Author", false)
+
+        every { bookPort.getAllBooks() } returns listOf(existingBook)
+        every { bookPort.isBookReserved(existingBook.title) } returns false
+        every { bookPort.reserveBook(existingBook.title) } answers {
+            existingBook = existingBook.copy(reserved = true)
+        }
+
+        bookUseCase.reserveBook(existingBook.title)
+
+        verify { bookPort.reserveBook(existingBook.title) }
+
+        assertThat(existingBook.reserved).isTrue()
+    }
+
+    @Test
+    fun `reserveBook throws BookReservedException when book is already reserved`() {
+        val existingBook = Book("test", "Author", true) // Book is already reserved
+
+        every { bookPort.getAllBooks() } returns listOf(existingBook)
+        every { bookPort.isBookReserved(existingBook.title) } returns true // Book is already reserved
+
+        assertFailure {
+            bookUseCase.reserveBook(existingBook.title)
+        }
+            .isInstanceOf(BookReservedException::class)
+
+
+        // Ensure that bookPort's reserveBook method was not called
+        verify(exactly = 0) { bookPort.reserveBook(existingBook.title) }
+    }
+
+    @Test
+    fun `isBookReserved returns true when book is reserved`() {
+        val bookTitle = "ReservedBook"
+        every { bookPort.isBookReserved(bookTitle) } returns true
+
+        val isReserved = bookUseCase.isBookReserved(bookTitle)
+
+        assertThat(isReserved).isTrue()
+        // Verify that bookPort's isBookReserved method was called with the provided title
+        verify { bookPort.isBookReserved(bookTitle) }
+    }
+
+    @Test
+    fun `isBookReserved returns false when book is not reserved`() {
+        val bookTitle = "NotReservedBook"
+        every { bookPort.isBookReserved(bookTitle) } returns false
+
+        val isReserved = bookUseCase.isBookReserved(bookTitle)
+
+        assertThat(isReserved).isFalse()
+
+        verify { bookPort.isBookReserved(bookTitle) }
     }
 }
--- src/testIntegration/kotlin/com/jicay/bookmanagement/infrastructure/driver/web/BookControllerIT.kt
+++ src/testIntegration/kotlin/com/jicay/bookmanagement/infrastructure/driver/web/BookControllerIT.kt
@@ -1,5 +1,9 @@
 package com.jicay.bookmanagement.infrastructure.driver.web
 
+import assertk.assertFailure
+import assertk.assertions.hasMessage
+import assertk.assertions.isInstanceOf
+import com.jicay.bookmanagement.domain.exceptions.BookReservedException
 import com.jicay.bookmanagement.domain.model.Book
 import com.jicay.bookmanagement.domain.usecase.BookUseCase
 import com.ninjasquad.springmockk.MockkBean
@@ -29,12 +33,9 @@ class BookControllerIT {
     //TODO SHOULD FAIL WHEN EXPECTED CONTENT DOES NOT HAVE "RESERVED"
     @Test
     fun `rest route get all books`() {
-        // GIVEN
         every { bookUseCase.getAllBooks() } returns listOf(Book("A", "B", true))
 
-        // WHEN
         mockMvc.get("/books")
-        //THEN
             .andExpect {
                 status { isOk() }
                 content { content { APPLICATION_JSON } }
@@ -127,5 +128,37 @@ class BookControllerIT {
         }
 
         verify(exactly = 0) { bookUseCase.addBook(any()) }
+    }
+    @Test
+    fun `rest route reserve book should reserve a book`() {
+        val bookTitle = "Les miserables"
+
+        every { bookUseCase.isBookReserved(bookTitle) } returns false
+        every { bookUseCase.reserveBook(bookTitle) } answers {}
+
+        mockMvc.post("/books/$bookTitle/reserve"){
+            contentType = APPLICATION_JSON
+            accept = APPLICATION_JSON
+        }.andExpect {
+            status { isOk() }
+        }
+    }
+
+    @Test
+    fun `rest route reserve book should throw Exception if book is already reserved`() {
+        val bookTitle = "Les miserables"
+
+        every { bookUseCase.isBookReserved(bookTitle) } returns true
+        every { bookUseCase.reserveBook(bookTitle) } answers { throw BookReservedException("") }
+
+        assertFailure {
+            mockMvc.post("/books/$bookTitle/reserve"){
+                contentType = APPLICATION_JSON
+                accept = APPLICATION_JSON
+            }.andExpect {
+                status { isBadRequest() }
+            }
+        }
+            .isInstanceOf(Exception::class)
     }
 }
